cae3853309afa8d445557f164b190df9de1d011a
DoYourLove <xyf15620168540@163.com>
2020-09-23 21:38:57 +0800

    [XiaoYiFan] feat : switch menu when login and logout

--- src/views/Profile/index.js
+++ src/views/Profile/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Profile extends Component {
   render() {
@@ -6,14 +7,18 @@ class Profile extends Component {
       <div className="main-view profile">
         <h2>个人信息</h2>
         <ul>
-          <li>用户名：</li>
-          <li>用户id：</li>
+          <li>用户名：{this.props.userInfo.name}</li>
+          <li>用户id：{this.props.userInfo.id}</li>
           <li>用户头像：</li>
-          <img src="" alt="头像" />
+          <img src={this.props.userInfo.avatar} alt="头像" />
         </ul>
       </div>
     );
   }
 }
 
-export default Profile;
+const mapStateToProps = ({ userInfo }) => ({
+  userInfo
+});
+
+export default connect(mapStateToProps)(Profile);
