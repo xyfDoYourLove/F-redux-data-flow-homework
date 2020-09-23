29f6cfc6ab5255350525547e51e985130d30f2c7
DoYourLove <xyf15620168540@163.com>
2020-09-23 21:35:28 +0800

    [XiaoYiFan] feat : switch menu when login and logout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
 import Header from './layout/Header';
 import Menu from './layout/Menu';
 import Home from './views/Home';
 import { menuOptions } from './utils';
 import './App.scss';
+import { getUserInfo } from './actions';
 
 class App extends Component {
   render() {
+    const permissions = this.props.isLogin ? this.props.userInfo.permissions : ['REPORT'];
+    console.log(this.props.isLogin);
     return (
       <div className="app">
         <Router>
@@ -18,11 +22,13 @@ class App extends Component {
               <Route exact path="/">
                 <Home />
               </Route>
-              {menuOptions.map(({ path, component }) => (
-                <Route key={path} path={path}>
-                  {component}
-                </Route>
-              ))}
+              {menuOptions
+                .filter(item => permissions.includes(item.permissionCode))
+                .map(({ path, component }) => (
+                  <Route key={path} path={path}>
+                    {component}
+                  </Route>
+                ))}
               <Route path="*">
                 <Home />
               </Route>
@@ -34,4 +40,9 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = ({ isLogin, userInfo }) => ({
+  isLogin,
+  userInfo
+});
+
+export default connect(mapStateToProps)(App);
--- src/layout/Menu.js
+++ src/layout/Menu.js
@@ -1,3 +1,4 @@
+import { connect } from 'react-redux';
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { menuOptions } from '../utils';
@@ -5,21 +6,29 @@ import './Menu.scss';
 
 class Menu extends Component {
   render() {
+    const permissions = this.props.isLogin ? this.props.userInfo.permissions : ['REPORT'];
     return (
       <nav className="menu">
         <ul>
           <li className="nav-item">
             <Link to="/">首页</Link>
           </li>
-          {menuOptions.map(({ name, path }) => (
-            <li className="nav-item" key={name}>
-              <Link to={path}>{name}</Link>
-            </li>
-          ))}
+          {menuOptions
+            .filter(item => permissions.includes(item.permissionCode))
+            .map(({ name, path }) => (
+              <li className="nav-item" key={name}>
+                <Link to={path}>{name}</Link>
+              </li>
+            ))}
         </ul>
       </nav>
     );
   }
 }
 
-export default Menu;
+const mapStateToProps = ({ isLogin, userInfo }) => ({
+  isLogin,
+  userInfo
+});
+
+export default connect(mapStateToProps)(Menu);
